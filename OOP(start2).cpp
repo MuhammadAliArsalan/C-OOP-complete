d4cd64be71de6fe76192fbc9acb9c01b1295af62
Muhammad Ali Arsalan <144929663+MuhammadAliArsalan@users.noreply.github.com>
2024-11-25 12:12:39 +0500

    Update OOP(start2).cpp

--- OOP(start2).cpp
+++ OOP(start2).cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 class Student{
-	private:  //acess specifier
+	private:  // Access specifier: private data members are not directly accessible outside the class
 		string stdName;
 		double CGPA;
 		
-	public:   //acess specifer
+	public:   // Access specifier: public methods are accessible outside the class
+
 		void setName(string name,double grades){
 			stdName=name;
 			CGPA=grades;
@@ -19,7 +20,7 @@ class Student{
 int main(){
 	cout<<"\t\t\tDETAILS"<<endl;
 	Student S1;
-	S1.setName("M.Ali Arslan",3.097);
+	S1.setName("M.Ali Arslan",3.097);  // Setting the student's name and CGPA
 	S1.displayName();
 	return 0;
 }

25331a1cca6ef822fda840bb390d30724f5b1a60
HasanCT282 <hasanarsalan781@gmail.com>
2024-12-19 12:45:21 +0500

    Committing files from tut3 to tut7

--- tut7.java
+++ tut7.java
@@ -34,8 +34,6 @@ public class tut7{
 }*/
 import java.util.Scanner;
 
-//import javax.swing.plaf.synth.SynthStyle;
-
 abstract class Methods{
     abstract void getStdDetail();
     abstract void setGPs();
@@ -45,7 +43,6 @@ class Result extends Methods{
     private int sizeOfArray;
     private int[] marks;
 
-    
     @Override
     public void setGPs(){
         Scanner scan=new Scanner(System.in);
@@ -76,13 +73,12 @@ class Result extends Methods{
     private int TotalMarks=0;
     private double Percent;
     private int MaxMarks=550;
+
     @Override
     public void calcPercent(){
         System.out.print("\nThe Total marks of "+sizeOfArray+" subject are ");
         for(int i=0;i<sizeOfArray;i++){
-            
-            TotalMarks=TotalMarks+marks[i];
-            
+            TotalMarks=TotalMarks+marks[i];     
         }
         Percent=((double)TotalMarks/MaxMarks)*100;
 
@@ -98,9 +94,6 @@ public class tut7{
         System.out.print("-----------------");
         result.setGPs();
         System.out.print("-----------------");
-        result.calcPercent();
-
-        
+        result.calcPercent();   
     }
-
 }
